3be71cc53e1fe5c27df038148c11caa9cc01e698
Gaz Liddon MBR13 <gaz@gazaxian.com>
2014-12-09 14:51:40 +0000

    broke it

--- src/cljs/cheg/core.cljs
+++ src/cljs/cheg/core.cljs
@@ -2,20 +2,136 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
-(defonce app-state (atom {:text "Gaz really says Hello Chestnut!"}))
 
-(def canvas-dims  #js {:width 200 :height 300 })
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn log [  a]
+  (.log js/console a) )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn draw-blob [ctx x y w h col]
+  (doto ctx
+    (aset "fillStyle" col)
+    (.moveTo x y)
+    (.fillRect x y w h) ))
+
+(defn draw-objs [ctx objs]
+  (doseq [{:keys [x y w h col]} o objs]
+    (draw-blob ctx x y w h col)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn update-vels [ {:keys [x y xv yv]} obj ]
+  (assoc
+    obj
+    :x (+ x xv) :y (+ y yv)))
+
+(defn home [cx cy {:keys [x y xv yv]} obj ]
+  (let scalefn (fn [p pv cp] (+ pv (* (- p cp) 0.1)))
+    (assoc
+      obj
+      :xv (scalefn x xv cx)
+      :yv (scalefn y yv cy))))
+
+(defn update-objs [player objs ]
+  (let [{:keys [px py]} player]
+    (vec 
+      (->> objs
+           (map update-vels)
+           (map #(home px py) )))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn update-state [{:keys [objs
+                            player]} state]
+  (-> state
+      (assoc :objs #(update-objs player objs %))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn render-all [ app owner node-ref ]
+  (let
+
+    [ surface  (om/get-node owner node-ref)
+      width    (.-width surface)
+      height   (.-height surface)
+      objs     (:objs app) ]
+
+    (doto
+      (.getContext surface "2d")
+      (aset "fillStyle" "grey")
+      (.fillRect 0 0 width height)
+      (draw-objs objs))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn header [ app owner ]
+  (reify
+    om/IRender
+    (render [ this ]
+      (dom/h1 nil (:title app)))))
+
+(defn canvas [ app owner ]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (js/setInterval
+        (fn [] (om/update! update-state)) 100)
+      )
+
+    om/IDidMount
+    (did-mount [_]
+      (render-all app owner "surface-ref"))
+
+    om/IDidUpdate
+    (did-update [_ _ _]
+      (render-all app owner "surface-ref"))
+
+    om/IRender
+    (render [_]
+      (dom/canvas #js {:className "canv"
+                       :id        "surface"
+                       :ref       "surface-ref" }))))
+
+(defn container [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div
+        #js {:className "container"}
+        (om/build header app)
+        (om/build canvas app)
+        (dom/p nil "Bottom")))))
+
+(defn page [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div
+        nil
+        (dom/h1 nil (:text app))
+        (om/build container app)))))
+
+(defonce app-state
+  (atom
+    {
+     :title "Chucky Egg Online"
+     :center-x 100
+     :center-y 100
+
+     :player {
+              :x 100
+              :v 100
+              :xv 0
+              :yv 0
+              }
+
+     :objs [
+            {:x 30 :y 20 :yv 0 :xv 0 :col "yellow"}
+            {:x 31 :y 30 :yv 0 :xv 0 :col "blue"}
+            {:x 35 :y 10 :yv 0 :xv 0 :col "black"}
+            {:x 30 :y 90 :yv 0 :xv 0 :col "white"}
+            ] } ))
 
 (defn main []
   (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (dom/div nil
-                   (dom/h1 nil (:text app))
-                   (dom/p nil (:text app))
-                   (dom/canvas canvas-dims )
-                   (dom/p nil "yeah!") ))))
+    page
     app-state
     {:target (. js/document (getElementById "app"))}))
