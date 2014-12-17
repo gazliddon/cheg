6411cc5c47d7d73e0f624558dd07d198efda2470
Gaz Liddon FatGaz <gaz@gazaxian.com>
2014-12-17 06:51:46 +0000

    Wip

--- spec/clj/cheg/statemachine_spec.clj
+++ spec/clj/cheg/statemachine_spec.clj
@@ -7,9 +7,11 @@
 ;; Set of state events and a state table
 (defn go-create [object ev & args]
   (-> object
-      (assoc :pos [0 0]
+      (assoc 
+             :pos [0 0]
              :lives 3)
       (ev :done)))
+
 
 ( defn go-standing [object & args]
   (-> object
@@ -17,6 +19,8 @@
         :vel [0 0]
         :anim :stand-anim
         :state :standing)))
+
+(println (go-create {} go-standing))
 
 (defn go-walk [object & args]
   (-> object
@@ -53,47 +57,86 @@
    :dieing   {:done             go-finished? } })
 
 (def test-events
-  [:create
-   :done
+  [:create               ; nothing -> created
+   :done                 ; ignored
+   :joypad               ; standing -> :walking
+   :enemy-collision      ; walking ->
+   :joypad
+   :joypad
    :joypad
    :enemy-collision
-   :joypad
-   :joypad
-   :joypad
    :enemy-collision
-   :enemy-collision
-   :done
-   ])
+   :done ])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Testing support routines
-(defn transition-record [event prev-state next-state]
-  (let [ev-info (str event " " (:state prev-state) " -> "  (:state next-state))]
-    (if (= prev-state next-state)
-      (str ev-info "\n\tNO CHANGE\n")
-      (str ev-info "\n\t"prev-state "\n\t" next-state "\n") 
-      )))
+(defn join-lines [str-array]
+  (reduce #(str %1 (apply str %2) "\n") "" str-array))
+
+(defn transition-report [event old-object new-object]
+  (let [CR "\n"]
+    (str
+      "Event: " event ": " (:state old-object)" -> " (:state new-object) CR
+      old-object CR
+      new-object CR
+      "Equal? " (= new-object old-object))))
 
 (defn next-event [{:keys [ changes object ] :as accum} event]
   (let [new-object (process-event fsm-table object event) ]
-    (-> accum
-        (assoc :object new-object
-               :changes (conj
-                          changes
-                          [(transition-record event object  new-object)])))))
+    (assoc
+      accum
+      :object new-object
+      :changes (conj
+                 changes 
+                 {:event event
+                  :old-object object
+                  :new-object new-object
+                  :object-change? (= new-object object)}))))
 
-(def runned-events
+(def run-results
   (let [starter {:changes [] :object {:state :nothing}}]
     (reduce next-event starter test-events)))
-(println "")
-(doseq [changes (:changes runned-events)]
-  (println changes))
-(println "")
-(println "End State")
-(println (:object runned-events))
 
+(def event-log (:changes run-results))
+(def ignored-events (filter #( :object-change? %1) event-log))
+(def effecting-events (filter #(complement ( :object-change? %1)) event-log))
+
+(def final-obj (:object run-results))
+
+
+(println (take 40 (repeat "-")))
+(println "Events that change the object's state")
+(doseq [{:keys [event old-object new-object]} effecting-events  ]
+  (println (transition-report event old-object new-object )))
+
+; (println (take 40 (repeat "-")))
+; (println "Events that had no effect")
+; (doseq [{:keys [event old-object new-object]} ignored-events]
+;   (println (transition-report event old-object new-object )))
+
+(doseq [e event-log]
+  (println e))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; And the tests
+(describe "Should have been an exact number of events"
+          (it "should have x events"
+              (should= 10 (count event-log))))
+
+(describe "Should have been an exact number of actual state changes"
+          (it "should have x state changes"
+              (should= 10 (count effecting-events))))
+
+(describe "Should have been an exact number of actual ignored events"
+          (it "should have x ignored events"
+              (should= 4 (count ignored-events))))
+
+(describe "The state of the final obj after the state run"
+          (it "The final object should be this"
+              (should= {:anim :die-anim
+                        :vel [1 0]
+                        :pos [0 0]
+                        :lives 0
+                        :state :game-over} final-obj)))
 
--- src/cljx/cheg/statemachine.cljx
+++ src/cljx/cheg/statemachine.cljx
@@ -6,7 +6,11 @@
   (get-in table [state event] state-identity))
 
 (defn process-event [ table {:keys [state] :as object} event & args]
-  (let [ev-fun (partial process-event table)
+  (let [ev-fun (fn [object event & args] (apply process-event table object event args))
         action-fn (get-event-action table state event)
-        new-obj  (action-fn object ev-fun state args)]
+        new-obj   (apply action-fn object ev-fun state args) ]
+    (println (str "event: " event))
+    (println (str "old obj" object))
+    (println (str "new obj" new-obj))
+    (println "")
     new-obj))
